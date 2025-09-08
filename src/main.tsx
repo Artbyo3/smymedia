e544543809973e59435e78c2a3bbd82f4d8703b5
o3-w <o3artcontact@gmail.com>
2025-09-07 21:08:40 -0400

    Update files

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
+
 import './index.css'
 import App from './App.tsx'
 import { AppProvider } from '@/context/AppContext'
@@ -8,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppProvider>
       <App />
+      <Analytics />
     </AppProvider>
   </StrictMode>,
 )
